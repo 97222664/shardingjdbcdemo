90afa542ec9047b72ff1a8e4d6f32439bfd62761
qianyucheng <97222664@qq.com>
2021-02-04 15:42:04 +0800

    [update]

--- src/test/java/com/example/shardingjdbcdemo/ShardingjdbcdemoApplicationTests.java
+++ src/test/java/com/example/shardingjdbcdemo/ShardingjdbcdemoApplicationTests.java
@@ -21,78 +21,78 @@ class ShardingjdbcdemoApplicationTests {
     @Resource
     private UserMapper userMapper;
 
-
-
-    @Test
-    void addUserDb() {
-        log.info("========================测试垂直分库========================");
-        TUser user = new TUser();
-
-        userMapper.insert(user);
-    }
-
-    @Test
-    void updateUserDb() {
-        log.info("========================测试垂直分库========================");
-        QueryWrapper<TUser> wrapper = new QueryWrapper<>();
-        wrapper.eq("user_id", 563750796609454081L);
-        TUser user = new TUser();
-
-        userMapper.update(user, wrapper);
-    }
-
-    @Test
-    void deleteUserDb() {
-        log.info("========================测试垂直分库========================");
-        QueryWrapper<TUser> wrapper = new QueryWrapper<>();
-        wrapper.eq("user_id", 563750796609454081L);
-        userMapper.delete(wrapper);
-    }
-
-    @Test
-    void getUserDb() {
-        log.info("========================测试垂直分库========================");
-        QueryWrapper<TUser> wrapper = new QueryWrapper<>();
-        wrapper.eq("user_id", 563776626383388673L);
-        TUser user = userMapper.selectOne(wrapper);
-        System.out.println(user);
-    }
-
-
-    @Test
-    void addCourseDb() {
-        log.info("========================测试水平分库========================");
-        for (int i = 0; i < 10; i++) {
-            Course course = new Course();
-            course.setUserId(100L + i);
-            course.setStatus(String.valueOf(i));
-            courseMapper.insert(course);
-        }
-    }
-
-
-    @Test
-    void addCourse() {
-        log.info("========================测试水平分表========================");
-        for (int i = 0; i < 10; i++) {
-            Course course = new Course();
-            course.setUserId(100L);
-            course.setStatus(String.valueOf(i));
-
-            courseMapper.insert(course);
-        }
-    }
-
-    @Test
-    void getCourse() {
-        log.info("========================测试水平分表========================");
-        QueryWrapper<Course> wrapper = new QueryWrapper<>();
-        wrapper.eq("cid", 563729630305976320L);
-
-        Course course = courseMapper.selectOne(wrapper);
-
-        System.out.println(course);
-
-    }
+//
+//
+//    @Test
+//    void addUserDb() {
+//        log.info("========================测试垂直分库========================");
+//        TUser user = new TUser();
+//
+//        userMapper.insert(user);
+//    }
+//
+//    @Test
+//    void updateUserDb() {
+//        log.info("========================测试垂直分库========================");
+//        QueryWrapper<TUser> wrapper = new QueryWrapper<>();
+//        wrapper.eq("user_id", 563750796609454081L);
+//        TUser user = new TUser();
+//
+//        userMapper.update(user, wrapper);
+//    }
+//
+//    @Test
+//    void deleteUserDb() {
+//        log.info("========================测试垂直分库========================");
+//        QueryWrapper<TUser> wrapper = new QueryWrapper<>();
+//        wrapper.eq("user_id", 563750796609454081L);
+//        userMapper.delete(wrapper);
+//    }
+//
+//    @Test
+//    void getUserDb() {
+//        log.info("========================测试垂直分库========================");
+//        QueryWrapper<TUser> wrapper = new QueryWrapper<>();
+//        wrapper.eq("user_id", 563776626383388673L);
+//        TUser user = userMapper.selectOne(wrapper);
+//        System.out.println(user);
+//    }
+//
+//
+//    @Test
+//    void addCourseDb() {
+//        log.info("========================测试水平分库========================");
+//        for (int i = 0; i < 10; i++) {
+//            Course course = new Course();
+//            course.setUserId(100L + i);
+//            course.setStatus(String.valueOf(i));
+//            courseMapper.insert(course);
+//        }
+//    }
+//
+//
+//    @Test
+//    void addCourse() {
+//        log.info("========================测试水平分表========================");
+//        for (int i = 0; i < 10; i++) {
+//            Course course = new Course();
+//            course.setUserId(100L);
+//            course.setStatus(String.valueOf(i));
+//
+//            courseMapper.insert(course);
+//        }
+//    }
+//
+//    @Test
+//    void getCourse() {
+//        log.info("========================测试水平分表========================");
+//        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+//        wrapper.eq("cid", 563729630305976320L);
+//
+//        Course course = courseMapper.selectOne(wrapper);
+//
+//        System.out.println(course);
+//
+//    }
 
 }
